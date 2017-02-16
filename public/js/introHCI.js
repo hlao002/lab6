101908443105db915d74ee46aa07b536eb9241de
Henry Lao <henrylao1213@gmail.com>
2017-02-16 04:47:31 -0800

    completed lab

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -26,7 +26,17 @@ function addProjectDetails(e) {
 	// get rid of 'project' from the front of the id 'project3'
 	var idNumber = projectID.substr('project'.length);
 
+	$.get('/project/' + idNumber, addProject);
 	console.log("User clicked on project " + idNumber);
+}
+
+function addProject(result) {
+  var projectHTML =
+    '<img src="' + result.image + '" class="detailsImage">' +
+    '<h4>' + result.title + '</h4>' + '<p><small>' + result.date + 
+    '</small></p><br /><p>' + result.summary + '</p>';
+
+  $("#project" + result.id + " .details").html(projectHTML);
 }
 
 /*
@@ -35,4 +45,16 @@ function addProjectDetails(e) {
  */
 function randomizeColors(e) {
 	console.log("User clicked on color button");
+	e.preventDefault();
+	$.get('/palette', createColors);
+}
+
+function createColors(result) {
+	var colors = result.colors.hex;
+	$('body').css('background-color', colors[0]);
+	$('.thumbnail').css('background-color', colors[1]);
+	$('h1, h2, h3, h4, h5, h5').css('color', colors[2]);
+	$('p').css('color', colors[3]);
+	$('.project img').css('opacity', .75);
+
 }
